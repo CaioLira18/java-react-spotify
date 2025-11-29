c222fef8313053add5af09e9c0e4644856fa2945
CaioLira18 <caiozao1212@gmail.com>
2025-11-29 00:51:30 -0300

    [feat] - First Commit

---
+++ src/test/java/br/com/caio/spotify/application/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.spotify.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
