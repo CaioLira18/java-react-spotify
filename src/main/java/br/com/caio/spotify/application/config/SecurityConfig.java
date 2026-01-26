5ebe62765c97d243c15ec1e0d74263244c99089a
CaioLira18 <caiozao1212@gmail.com>
2026-01-25 22:28:10 -0300

    [feat] - Update

--- src/main/java/br/com/caio/spotify/application/config/SecurityConfig.java
+++ src/main/java/br/com/caio/spotify/application/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORRIGIDO: usar a configuração
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll()
