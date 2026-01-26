e31770426a3b4dd8f2f81e07339231dece48ca12
CaioLira18 <caiozao1212@gmail.com>
2026-01-25 21:36:49 -0300

    [feat] - Update Cross Origin

--- src/main/java/br/com/caio/spotify/application/controllers/UserController.java
+++ src/main/java/br/com/caio/spotify/application/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.caio.spotify.application.services.UserService;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("https://java-react-spotify.onrender.com/")
 public class UserController {
 
     @Autowired
